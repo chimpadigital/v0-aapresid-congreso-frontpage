d4d941af15ad9b68578d993af8e78b0af3d56983
Jimena Martinez <93katu@gmail.com>
2025-05-12 15:37:01 -0300

    agrgadas algunas animaciones y arreglo en fetch de logos medios e institucionales

--- app/api/sponsors/route.ts
+++ app/api/sponsors/route.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export async function GET() {
   try {
-    const [a, b, c, d, e, f] = await Promise.all([
+    const [a, b, c, d, institucion, medios] = await Promise.all([
       axios.get("https://api.congreso.v1.franco.in.net/api/logos?categoryId=8"),
       axios.get("https://api.congreso.v1.franco.in.net/api/logos?categoryId=2"),
       axios.get("https://api.congreso.v1.franco.in.net/api/logos?categoryId=3"),
@@ -17,8 +17,8 @@ export async function GET() {
       category2: b.data.data,
       category3: c.data.data,
       category4: d.data.data,
-      institucion: e.data.data,
-      medios: e.data.data,
+      institucion: institucion.data.data,
+      medios: medios.data.data,
     });
   } catch (error) {
     console.error("API error:", error);
