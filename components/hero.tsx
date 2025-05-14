1972d4f645683e2a0cf87e4297fcfbe260b1a374
chimpadigital <sdesigncba@gmail.com>
2025-05-14 15:59:26 -0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,7 +13,6 @@ export function Hero() {
 
   const t = useTranslations("hero");
 
-
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-8 pt-16 md:pt-32">
       <div className="max-w-3xl">
