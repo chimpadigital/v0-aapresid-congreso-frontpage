4d5b8314dc206b7e1de91c2d6f081bb14759be15
Jimena Martinez <93katu@gmail.com>
2025-06-25 15:17:46 -0300

    agregado wmbwbido comercial

--- components/comercial/werapper-comercial.tsx
+++ components/comercial/werapper-comercial.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { HeroSection } from "../hero-section";
 import DescargaComercialV2 from "./descarga-comercial-v2";
@@ -10,10 +10,35 @@ const WrapperComercial = () => {
   return (
     <>
       <HeroSection
-        title1={t('comercial')}
+        title1={t("comercial")}
         backgroundImage="/images/comercial/hero.webp"
       />
       <DescargaComercialV2 />
+      <div className="mx-auto max-w-7xl mt-10 overflow-hidden rounded-[20px]"
+        style={{
+          position: "relative",
+          paddingTop: "max(60%,326px)",
+          height: 0,
+          width: "100%",
+        }}
+      >
+        <iframe
+          allow="clipboard-write"
+          sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms"
+          allowFullScreen={true}
+          style={{
+            position: "absolute",
+            border: "none",
+            width: "100%",
+            height: "100%",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          }}
+          src="https://e.issuu.com/embed.html?d=tarifario_congreso_aapresid_2025&u=aapresid"
+        ></iframe>
+      </div>
       <BannerContacto />
     </>
   );
