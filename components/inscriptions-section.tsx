899b46453eb51b824bd38a901c11698dc5bd538f
Jimena Martinez <93katu@gmail.com>
2025-06-26 13:11:14 -0300

    toooltips agregados

--- components/inscriptions-section.tsx
+++ components/inscriptions-section.tsx
@@ -1,12 +1,10 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
 import ArrowRounded from "./icons/ArrowRounded";
 import { useLocale, useTranslations } from "next-intl";
-import * as motion from "motion/react-client";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { useEffect, useState } from "react";
-import ArrowBack from "./icons/ArrowBack";
+import { Tooltip } from "react-tooltip";
 
 export function InscriptionsSection() {
   const t = useTranslations("inscriptions");
@@ -54,13 +52,29 @@ export function InscriptionsSection() {
             <div className="flex justify-between border-b border-primary pb-4">
               <p className="flex-1 text-lg font-bold">NO SOCIO</p>
               <p className="flex-1 text-lg">
-                500.000  <span className="font-medium">ARS</span>
+                500.000 <span className="font-medium">ARS</span>
               </p>
             </div>
 
             <div className="flex justify-between border-b border-primary pb-4">
               <div className="flex flex-1 items-start">
-                <p className="mr-2 text-lg font-bold">ESTUDIANTE</p>
+                <p className="mr-2 flex items-center gap-2 text-lg font-bold">
+                  ESTUDIANTE{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="19"
+                    height="20"
+                    viewBox="0 0 19 20"
+                    fill="none"
+                    data-tooltip-id="tooltip-adherente"
+                    data-tooltip-place="top"
+                  >
+                    <path
+                      d="M8.70837 13.959H10.2917V9.20898H8.70837V13.959ZM9.50004 7.62565C9.72435 7.62565 9.9125 7.54965 10.0645 7.39765C10.2165 7.24565 10.2922 7.05776 10.2917 6.83398C10.2912 6.61021 10.2152 6.42232 10.0637 6.27032C9.91224 6.11832 9.72435 6.04232 9.50004 6.04232C9.27574 6.04232 9.08785 6.11832 8.93637 6.27032C8.7849 6.42232 8.7089 6.61021 8.70837 6.83398C8.70785 7.05776 8.78385 7.24592 8.93637 7.39844C9.0889 7.55097 9.27679 7.62671 9.50004 7.62565ZM9.50004 17.9173C8.4049 17.9173 7.37574 17.7094 6.41254 17.2935C5.44935 16.8776 4.6115 16.3137 3.899 15.6017C3.1865 14.8897 2.62257 14.0519 2.20721 13.0882C1.79185 12.1244 1.5839 11.0953 1.58338 10.0007C1.58285 8.90604 1.79079 7.87687 2.20721 6.91315C2.62363 5.94943 3.18756 5.11158 3.899 4.39961C4.61044 3.68764 5.44829 3.12371 6.41254 2.70782C7.37679 2.29193 8.40596 2.08398 9.50004 2.08398C10.5941 2.08398 11.6233 2.29193 12.5875 2.70782C13.5518 3.12371 14.3896 3.68764 15.1011 4.39961C15.8125 5.11158 16.3767 5.94943 16.7937 6.91315C17.2106 7.87687 17.4183 8.90604 17.4167 10.0007C17.4151 11.0953 17.2072 12.1244 16.7929 13.0882C16.3786 14.0519 15.8146 14.8897 15.1011 15.6017C14.3875 16.3137 13.5497 16.8779 12.5875 17.2943C11.6254 17.7107 10.5962 17.9184 9.50004 17.9173Z"
+                      fill="#2D3D34"
+                    />
+                  </svg>
+                </p>
                 <button
                   aria-label="Información para estudiantes"
                   className="hover: transition-colors"
@@ -77,8 +91,28 @@ export function InscriptionsSection() {
             </div>
 
             <div className="flex justify-between pb-4">
-              <div className="flex flex-1">
-                <p className="mr-2 text-lg font-bold">ADHERENTE</p>
+              <div className="flex flex-1 items-start">
+                <p className="mr-2 flex items-center gap-2 text-lg font-bold">
+                  ADHERENTE
+                  <button
+                    data-tooltip-id="tooltip-estudiente"
+                    data-tooltip-place="top"
+                    type="button"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="19"
+                      height="20"
+                      viewBox="0 0 19 20"
+                      fill="none"
+                    >
+                      <path
+                        d="M8.70837 13.959H10.2917V9.20898H8.70837V13.959ZM9.50004 7.62565C9.72435 7.62565 9.9125 7.54965 10.0645 7.39765C10.2165 7.24565 10.2922 7.05776 10.2917 6.83398C10.2912 6.61021 10.2152 6.42232 10.0637 6.27032C9.91224 6.11832 9.72435 6.04232 9.50004 6.04232C9.27574 6.04232 9.08785 6.11832 8.93637 6.27032C8.7849 6.42232 8.7089 6.61021 8.70837 6.83398C8.70785 7.05776 8.78385 7.24592 8.93637 7.39844C9.0889 7.55097 9.27679 7.62671 9.50004 7.62565ZM9.50004 17.9173C8.4049 17.9173 7.37574 17.7094 6.41254 17.2935C5.44935 16.8776 4.6115 16.3137 3.899 15.6017C3.1865 14.8897 2.62257 14.0519 2.20721 13.0882C1.79185 12.1244 1.5839 11.0953 1.58338 10.0007C1.58285 8.90604 1.79079 7.87687 2.20721 6.91315C2.62363 5.94943 3.18756 5.11158 3.899 4.39961C4.61044 3.68764 5.44829 3.12371 6.41254 2.70782C7.37679 2.29193 8.40596 2.08398 9.50004 2.08398C10.5941 2.08398 11.6233 2.29193 12.5875 2.70782C13.5518 3.12371 14.3896 3.68764 15.1011 4.39961C15.8125 5.11158 16.3767 5.94943 16.7937 6.91315C17.2106 7.87687 17.4183 8.90604 17.4167 10.0007C17.4151 11.0953 17.2072 12.1244 16.7929 13.0882C16.3786 14.0519 15.8146 14.8897 15.1011 15.6017C14.3875 16.3137 13.5497 16.8779 12.5875 17.2943C11.6254 17.7107 10.5962 17.9184 9.50004 17.9173Z"
+                        fill="#2D3D34"
+                      />
+                    </svg>
+                  </button>
+                </p>
                 <button
                   aria-label="Información para adherentes"
                   className="hover: transition-colors"
@@ -146,6 +180,22 @@ export function InscriptionsSection() {
           </Link>
         </div>
       </section>
+      <Tooltip clickable id="tooltip-estudiente">
+        <h4 className="mb-4 text-base font-semibold tracking-wider">
+          Estudiante
+        </h4>
+        <p className="text-base tracking-wider">
+          Deberás adjuntar tu certificado de alumno regular
+        </p>
+      </Tooltip>
+      <Tooltip clickable id="tooltip-adherente">
+        <h4 className="mb-4 text-base font-semibold tracking-wider">
+          Adherente
+        </h4>
+        <p className="text-base tracking-wider">
+          Deberás adjuntar el convenio vigente
+        </p>
+      </Tooltip>
     </div>
   );
 }
